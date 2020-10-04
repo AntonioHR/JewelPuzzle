d35ee713ab2cd1d00f2e2a815ee0519a811b0c37
Antonio Harger Rivero <antonio.harger@gmail.com>
2020-10-04 01:47:31 -0300

    Cleaned up piece color class

--- Assets/_Project/Scripts/JewelPuzzle/Board/PieceColor.cs
+++ Assets/_Project/Scripts/JewelPuzzle/Board/PieceColor.cs
@@ -7,27 +7,6 @@ namespace AntonioHR.JewelPuzzle
     [CreateAssetMenu(menuName = "JewelPuzzle/PieceColor")]
     public class PieceColor: ScriptableObject
     {
-
-        //Inspector Variables
         public Sprite sprite;
-        
-        //Public Properties
-        
-        //Public Functions
-        
-        //Unity Messages
-        private void Start()
-        {
-        
-        }
-        
-        #region Private Functions
-        #endregion
-        
-        #region Private Properties
-        #endregion
-        
-        #region Private Variables
-        #endregion
     }
 }
